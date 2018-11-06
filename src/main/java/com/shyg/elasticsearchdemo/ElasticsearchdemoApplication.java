18dbf7fc905402b551271a9642c8d1ecde536144
wuyongfei-1 <1402218191@qq.com>
2018-11-06 22:07:11 +0800

    ElasticsearchDemo

---
+++ src/main/java/com/shyg/elasticsearchdemo/ElasticsearchdemoApplication.java
@@ -0,0 +1,12 @@
+package com.shyg.elasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchdemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/shyg/elasticsearchdemo/controller/CityController.java
@@ -0,0 +1,55 @@
+package com.shyg.elasticsearchdemo.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.shyg.elasticsearchdemo.entity.City;
+import com.shyg.elasticsearchdemo.service.CityESService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * this class by created wuyongfei on 2018/6/5 13:50
+ **/
+@RestController
+public class CityController {
+
+    @Autowired
+    private CityESService cityESService;
+
+    /**
+     * create index to Elastic Search
+     * @param indexName
+     * @return
+     */
+    @PostMapping("/index/save")
+    public String saveIndex(String indexName) {
+        try {
+            cityESService.buildIndex(indexName);
+            return "{\"success\":1}";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"success\":0}";
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    @PostMapping("/type/save")
+    public String saveType() {
+        JSONObject jsonObject = new JSONObject();
+        City city = new City();
+        city.setId(1L);
+        city.setName("shang");
+        String citys = jsonObject.toJSONString(city);
+        JSONObject jsonObject1 = JSONObject.parseObject(citys);
+        try {
+            cityESService.addTargetDataALL(jsonObject1, "gtu-test", "gtu", "1");
+            return "{\"success\":1}";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"success\":0}";
+        }
+    }
+}
